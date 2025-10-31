50ec11fb36dee6a5e9d5d8cf9756cc45feb0699a
Michael Rhoads <mrhoads14@outlook.com>
2025-10-30 21:43:57 -0400

    complete workshop steps

---
+++ index.js
@@ -0,0 +1,30 @@
+
+console.log("I'm here!");
+
+// step 1
+let code1;
+console.log(code1);
+
+// step 2
+code1 = 7 + 3;
+console.log(code1);
+
+// step 3
+let code2 = 100 / 2 - 10;
+console.log(code2);
+
+// step 4
+let code3 = 3 % 2 + 38;
+console.log(code3);
+
+// step 5
+let message = "The vault has been secured. The combination is: ";
+console.log(message);
+
+// step 6
+let codeA = code1 + "-" + code2 + "-" + code3;
+console.log(codeA);
+
+// step 7
+let codeB = `${code1}-${code2}-${code3}`;
+console.log(codeB);
